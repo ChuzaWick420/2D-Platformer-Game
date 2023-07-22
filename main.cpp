43589a3e51bfcd921d2cc315701aa203752f2443
ChuzaWick420 <zaiddadev@gmail.com>
2023-07-22 21:34:29 +0500

    Implemented Memory management using 'new' & 'delete' keywords
    Before, everything was displayed at top of each other, now they don't exist unless u want them to, also now we can move forward to creating new levels on the go and redirecting the level_ptr to point to it

--- main.cpp
+++ main.cpp
@@ -14,42 +14,23 @@ int main() {
 	//window
 	sf::RenderWindow game_window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Plateformer", sf::Style::Default);
 
-	//GUI
-	GUI game;
-
-	//resizes the GUI to fit the window
-	game.resize(sf::Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT));
-
-	Level level;
-	level.create("1.png", "map.png", sf::Vector2u(64, 64), sf::Vector2u(16, 16));
-	level.create_hitboxes(sf::Vector2u(64, 64));
-
 	//window icon
 	sf::Image w_icon;
 	w_icon.loadFromFile("assets/bullet.png");
 
 	game_window.setIcon(w_icon.getSize().x, w_icon.getSize().y, w_icon.getPixelsPtr());
 
-	Player player;
+	//GUI manager
+	GUI game;
 
-	player.player_sprite.setOrigin(sf::Vector2f(player.idle_t[0].getSize().x / 2, player.idle_t[0].getSize().y / 2));
+	//resizes the GUI to fit the window
+	game.resize(sf::Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT));
 
-	player.player_sprite.setPosition(64 * 6, 64 * 6);
+	//pointers pointing to objects of the game
+	Level* level_ptr = nullptr;
+	Player* player_ptr = nullptr;
 
-	sf::Vector2f hitbox_offset = {0 , 30};		//10 pixels for feet and head
-
-	player.create_hitbox(
-		{
-
-			player.player_sprite.getPosition().x - player.player_sprite.getGlobalBounds().width / 2,
-			player.player_sprite.getPosition().y - player.player_sprite.getGlobalBounds().height / 2 + hitbox_offset.y		//10 for head
-
-		},
-		{
-			player.player_sprite.getGlobalBounds().width,
-			player.player_sprite.getGlobalBounds().height - 10 - hitbox_offset.y		//10 for feet, 10 for head
-		}
-	);
+	sf::Vector2f hitbox_offset = { 0 , 30 };		//10 pixels for feet and head
 
 	//variables
 	sf::Clock game_ticks, player_ticks, fps, physics_clock;
@@ -64,11 +45,8 @@ int main() {
 	const int animation_frames_player = 10;
 	const int animation_frames_button = 4;
 	int current_frame_player = 0;
-	int current_frame_button = 0;
 
 	float gravity_coefficient = 0;
-
-	game_window.setView(level.camera);
 
 	//game loop
 	while (game_window.isOpen()) {
@@ -77,52 +55,89 @@ int main() {
 
 		while (game_window.pollEvent(window_events)){
 
-			if (window_events.type == sf::Event::Closed)
+			if (window_events.type == sf::Event::Closed) {
+
+				//delete the objects
+				if (level_ptr != nullptr && player_ptr != nullptr) {
+					delete level_ptr;
+					delete player_ptr;
+				}
+
 				game_window.close();
 
-		}
-
-		//handles physics timings
-		if (physics_clock.getElapsedTime().asMilliseconds() >= physics_timer) {
-
-			control(player);
-
-			//updates hitboxes
-			player.get_hitbox()->update(player.player_sprite, hitbox_offset);
-
-			//applies gravity to objects
-			apply_gravity(player, level, gravity_coefficient);
-
-			level.update(player, game_window);
-
-			physics_clock.restart();
+				return 0;
+			}
 
 		}
 
 		//handles game timings
 		if (game_ticks.getElapsedTime().asMilliseconds() >= game_update_timer) {
 
-			//animation
+			//sets the pointers according to the current state
+			switch (game.current_state) {
 			
-			//player
-			if (current_frame_player < animation_frames_player) {
-				player.animate(player.current_state, current_frame_player);
-				current_frame_player++;
-			}
+				case game.main_menu:
+					game.Play.animate(game_mouse);
+					break;
 
-			else
-				current_frame_player = 0;	//reset
-
-			if(game.current_state == game.main_menu)
-				game.Play.animate(game_mouse);
-
-			else if (game.current_state == game.level_selector) {
-				for (int i = 0; i < LEVELS_PER_ROW; i++) {
-					for (int j = 0; j < LEVELS_PER_COLUMN; j++) {
-						int index = i * LEVELS_PER_COLUMN + j;
-						game.levels[index].Level.animate(game_mouse);
+				case game.level_selector:
+					for (int i = 0; i < LEVELS_PER_ROW; i++) {
+						for (int j = 0; j < LEVELS_PER_COLUMN; j++) {
+							int index = i * LEVELS_PER_COLUMN + j;
+							game.levels[index].Level.animate(game_mouse);
+						}
 					}
-				}
+					break;
+
+				case game.level_screen:
+
+					//make the level if pointer was empty, otherwise skip the creation process
+					if (level_ptr == nullptr) {
+
+						level_ptr = new Level;
+
+						(*level_ptr).create("1.png", "map.png", sf::Vector2u(64, 64), sf::Vector2u(16, 16));
+						(*level_ptr).create_hitboxes(sf::Vector2u(64, 64));
+	
+						game_window.setView((*level_ptr).camera);
+
+					}
+
+					if (player_ptr == nullptr) {
+
+						player_ptr = new Player;
+
+						(*player_ptr).player_sprite.setOrigin(sf::Vector2f((*player_ptr).idle_t[0].getSize().x / 2, (*player_ptr).idle_t[0].getSize().y / 2));
+						(*player_ptr).player_sprite.setPosition(64 * 6, 64 * 6);
+
+						(*player_ptr).create_hitbox(
+							{
+
+								(*player_ptr).player_sprite.getPosition().x - (*player_ptr).player_sprite.getGlobalBounds().width / 2,
+								(*player_ptr).player_sprite.getPosition().y - (*player_ptr).player_sprite.getGlobalBounds().height / 2 + hitbox_offset.y		//10 for head
+
+							},
+							{
+								(*player_ptr).player_sprite.getGlobalBounds().width,
+								(*player_ptr).player_sprite.getGlobalBounds().height - 10 - hitbox_offset.y		//10 for feet, 10 for head
+							}
+						);
+
+					}
+
+					//animation
+
+					//player
+					if (current_frame_player < animation_frames_player) {
+						(*player_ptr).animate((*player_ptr).current_state, current_frame_player);
+						current_frame_player++;
+					}
+
+					else
+						current_frame_player = 0;	//reset
+
+			default:
+				break;
 			}
 
 			//moves between different GUI screens
@@ -133,14 +148,39 @@ int main() {
 
 		}
 
+		//handles physics timings
+		if (physics_clock.getElapsedTime().asMilliseconds() >= physics_timer && game.current_state == game.level_screen) {
+			
+			if (player_ptr != nullptr && level_ptr != nullptr) {
+				
+				control(*player_ptr);
+				
+				//updates hitboxes
+				(*player_ptr).get_hitbox()->update((*player_ptr).player_sprite, hitbox_offset);
+	
+				//applies gravity to objects
+				apply_gravity(*player_ptr, *level_ptr, gravity_coefficient);
+	
+				(*level_ptr).update(*player_ptr, game_window);
+			
+			}
+
+			physics_clock.restart();
+
+		}
+
 		//handles fps and drawing timings
 		if (fps.getElapsedTime().asMilliseconds() >= fps_timer) {
 
 			game_window.clear();
 
-			level.render(game_window);
-			player.render(game_window);
-			player.get_hitbox()->render(game_window);
+			if (level_ptr != nullptr)
+				(*level_ptr).render(game_window);
+
+			if (player_ptr != nullptr) {
+				(*player_ptr).render(game_window);
+				(*player_ptr).get_hitbox()->render(game_window);
+			}
 
 			game.render(game_window);
 
